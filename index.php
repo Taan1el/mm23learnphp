055bff3ed6a6bf0f40f6ff20d741a86b84615815
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2025-09-04 11:25:38 +0300

    boolean math

--- index.php
+++ index.php
@@ -1,34 +1,47 @@
 <?php
 
-$num = 1;
-$num = $num + 1;
-var_dump($num);
+$bool = true && false; //false
+var_dump($bool);
 
-$num = $num - 1;
-var_dump($num);
+$bool = true || false; //true
+var_dump($bool);
 
-$num = $num * 2;
-var_dump($num);
+$bool = !true; //false
+var_dump($bool);
 
-$num = $num / 2;
-var_dump($num);
+$bool = 10 > 5; //true
+var_dump($bool);
 
-$num = $num % 2;
-var_dump($num);
+$bool = 10 < 5; //false
+var_dump($bool);
 
-$num +=1;
-$num -=1;
-$num *=2;
-$num /=2;
-$num %=2;
-var_dump($num);
+$bool = 10 < 10; //false
+var_dump($bool);
 
-$num++;
-$num--;
-var_dump($num);
+$bool = 10 > 10; //false
+var_dump($bool);
 
-$num = $num **2;
-$num **=2;
-var_dump($num);
+$bool = 10 >= 10; //true
+var_dump($bool);
 
+$bool = 10 <= 10; //true
+var_dump($bool);
+
+$bool = 10 == 10; //true
+var_dump($bool);
+
+$bool = 10 == "10"; //true
+var_dump($bool);
+
+$bool = 10 === "10"; //false
+var_dump($bool);
+
+$bool = 10 != 10; //false
+var_dump($bool);
+
+$bool = 10 !== 10; //true
+var_dump($bool);
+
+$bool = (true && false || true && !false) && true; //true
+var_dump($bool);
 
