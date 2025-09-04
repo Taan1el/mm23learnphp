5f65642262ff15c02ebe2f11e1b042e883be9d1f
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2025-09-04 10:13:55 +0300

    variables and examples

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo ("Hello, World!\n");
+echo "Hello, World!\n";
+echo "\n";// new line
+$name = "TAANIEL!";
+echo "Hello, $name\n";
+$age = 18;
+echo "I am $age years old " . "and my name is $name\n";
+echo "\n";
+echo "Your name is $name and you are $age years old.\n";
+
+$var = 1;// integer
+var_dump($var);
+$var = 1.5;// float
+var_dump($var);
+$var = "Hello";// string 
+var_dump($var);
+$var = true;// boolean
+var_dump($var);
+$var = false;
+var_dump($var);
+$bar = null; // null
+var_dump($bar); 
+
+?> 
