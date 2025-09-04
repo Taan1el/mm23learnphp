50e0854bf9e937c9482d93769fb9e3b232cfb60e
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2025-09-04 10:42:48 +0300

    array of examples ;)

--- index.php
+++ index.php
@@ -1,26 +1,87 @@
 <?php
 
-echo ("Hello, World!\n");
-echo "Hello, World!\n";
-echo "\n";// new line
-$name = "TAANIEL!";
-echo "Hello, $name\n";
-$age = 18;
-echo "I am $age years old " . "and my name is $name\n";
-echo "\n";
-echo "Your name is $name and you are $age years old.\n";
+$array = array(1, 2, 3); // Old syntax
+var_dump($array);
+$array2 = [1, 2, 3]; // New syntax
+var_dump($array2);
 
-$var = 1;// integer
-var_dump($var);
-$var = 1.5;// float
-var_dump($var);
-$var = "Hello";// string 
-var_dump($var);
-$var = true;// boolean
-var_dump($var);
-$var = false;
-var_dump($var);
-$bar = null; // null
-var_dump($bar); 
+$array =[
+    1,
+    "Taaniel",
+   true,
+   [1,2,3],
+   3.4,
+];
+var_dump($array);
 
-?> 
+$array =[
+    "name" => "Taaniel",
+    "age" => 18,
+    "city" => "Tallinn",
+    "isMale" => true,
+    "isStudent" => true,
+];
+var_dump($array);
+
+$array =[
+    1,
+    2,
+    "name" => "Taaniel",
+    "age" => 18,
+    6,
+    "city" => "Tallinn",
+    "isMale" => true,
+    "isStudent" => true,
+    3,
+    4,
+    5,
+];
+var_dump($array);
+
+$array =[1, 2, 3];
+var_dump($array[1]); // 2
+
+$array =[
+    "name" => "Taaniel",
+    "age" => 18,
+    "city" => "Tallinn",
+    "isMale" => true,
+    "isStudent" => true,
+];
+var_dump($array["name"]); // Taaniel
+
+
+$array =[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+var_dump($array[1][2]); // 6
+
+$array =[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+var_dump($array[2][1]); // 8
+
+$array =[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+var_dump($array[0][0]);// 1
+var_dump($array[0][1]);// 2
+var_dump($array[0][2]);// 3
+var_dump($array[1][0]);// 4
+var_dump($array[1][1]);// 5
+
+$array = [1, 2, 3];
+var_dump($array);
+array_push($array, 4);
+array_push($array, 5, 6, 7);
+$array[] = 8;
+var_dump($array);
+
+$array[1] = "something else";
+var_dump($array);
