1eefc76f0eaa35df8256695938d444378984e113
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2025-09-05 14:15:27 +0300

    visibility examples

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <?php
 
-function sayHello(){
-    var_dump("Hello");
-}
+class Box {
+    public $height;
+    public $width;
+    public $length;
 
-sayHello();
+    public function __construct($height, $width, $length) {
+        $this->height = $height;
+        $this->width = $width;
+        $this->length = $length;
+    }
 
-function sayHelloToMe($name="Nameless"){
-    var_dump("Hello $name");
-}
-
-sayHelloToMe("Taaniel");
-sayHelloToMe("Tarvin");
-sayHelloToMe("Teet");
-sayHelloToMe("Tõnu");
-sayHelloToMe("Tõnis");
-sayHelloToMe();
-
-function sayNameAndAge($name, $age){
-    var_dump("Hello $name, you are $age years old");
-}
-
-sayNameAndAge("Taaniel", 18);
-sayNameAndAge("Tarvin", 22);
-sayNameAndAge("Teet", 24);
-sayNameAndAge("Tõnu", 26);  
-sayNameAndAge("Tõnis", 28);
-
-function recursion($i){
-    if($i < 11){
-        var_dump($i);
-        recursion($i + 1);
+public function setHeight($height){
+    if(is_numeric($height) && $height > 0){
+        $this->height = $height;
+    } else {
+        throw new Exception("Height must be a positive number.");
     }
 }
 
-recursion(1);
+    public function volume(){
+        return $this->height * $this->width * $this->length;
+    }
+}
+
+$box1 = new Box(10, 20, 30);
+$box1->setWidth(15);
+$volume1 = $box1->volume();
+var_dump($box1);
+var_dump($volume1);
+var_dump($box1->volume());
+
+$box2 = new Box(40, 50, 60);
+var_dump($box2);
+var_dump($box2->volume());
+
