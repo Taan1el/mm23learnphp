839b5dc8e1f4d74ddc85e07af26d185a6c815837
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2025-09-12 16:14:59 +0300

    Almost working dynamic posts

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+$title = "Main";
+ $posts = [
+    ['title' => 'Sample blog post', 'date' => 'January 1, 2024', 'author' => 'Mark', 'body' => 'NOT Cool world news!1'],
+    ['title' => 'Another blog post', 'date' => 'December 23, 2024', 'author' => 'Jacob', 'body' => 'NOT More cool world news!2'],
+    ['title' => 'cool blog', 'date' => 'December 17, 2023', 'author' => 'Chris', 'body' => 'NO, Even more cool world news!3'], 
+    ['title' => 'Another blog but cooler', 'date' => 'December 13, 2025', 'author' => 'Bill', 'body' => 'There Are Even more cool world news!4'],
+    ['title' => 'Awsome blog', 'date' => 'December 15, 2026', 'author' => 'Tom', 'body' => 'Even more Bad world news!5'],
+  ];
+?>
 
 <?php include 'partials/header.php'; ?>
     <main class="container">
