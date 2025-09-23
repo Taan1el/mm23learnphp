efd9dd42d0209a23f03df237ed97f6b44aa6e606
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2025-09-23 16:55:05 +0300

    correct cookies

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -9,16 +9,7 @@ class AuthController {
     }
 
     public function register() {
-        dump($_POST);
-        $user = User::where('email', $_POST['email']);
-        if(isset($user[0]) || !isset($_POST["email"]) || $_POST['password'] !== $_POST['password_confirm']) {
-            return header('Location: /register');
-        }
-        $user = new User();
-        $user->email = $_POST['email'];
-        $user->password = $_POST['password'];
-        $user->save();
-        header('Location: /login');
+
     }
 
     public function loginForm() {
