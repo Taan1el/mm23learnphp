ed597590c6e0a91e57ae92635efaf884f8df93b2
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2025-09-23 14:46:31 +0300

    all of router but without SQLite

---
+++ helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+function dump(...$args) {
+    echo '<pre>';
+    var_dump(...$args);
+    echo '</pre>';
+}
+
+function dd(...$args) {
+    dump(...$args);
+    die;
+}
+
+function view($viewName, $variables=[]) {
+    extract($variables);
+    include __DIR__ . "/views/$viewName.php";
+}
---
+++ routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Controllers\ArticleController;
+use App\Controllers\PublicController;
+use App\Router;
+
+Router::get('/', [PublicController::class, 'index']);
+
+Router::get('/us', [PublicController::class, 'us']);
+
+Router::get('/form', [PublicController::class, 'form']);
+Router::post('/answer', [PublicController::class, 'answer']);
+
+Router::get('/articles', [ArticleController::class, 'index']);
+Router::get('/articles/create', [ArticleController::class, 'create']);
+Router::post('/articles', [ArticleController::class, 'store']);
+Router::get('/articles/view', [ArticleController::class, 'view']);
+Router::get('/articles/edit', [ArticleController::class, 'edit']);
+Router::post('/articles/edit', [ArticleController::class, 'update']);
+Router::get('/articles/delete', [ArticleController::class, 'destroy']);
